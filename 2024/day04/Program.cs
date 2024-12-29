ac5eef4969529fb8bb07cfa9bef95f7e80b4077b
Phil Hohnstadt <gitlab.straining057@passmail.net>
2024-12-29 16:08:07 -0500

    initial commit of some 2024 puzzle solutions

---
+++ 2024/day04/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(partOne("input.txt"));
+        Console.WriteLine(partTwo("input.txt"));
+    }
+
+    public static int partOne(string filePath)
+    {
+        int xmasCount = 0;
+        char[] xmasChars = ['X','M','A','S'];
+        string[] inputLines = File.ReadAllLines(filePath);
+        List<(List<int>, List<int>)> checkOffsets = new()
+        {
+            ([0, 0, 0, 0], [0, 1, 2, 3]),        // Right
+            ([0, 1, 2, 3], [0, 1, 2, 3]),        // Down-Right
+            ([0, 1, 2, 3], [0, 0, 0, 0]),        // Down
+            ([0, 1, 2, 3], [0, -1, -2, -3]),     // Down-Left
+            ([0, 0, 0, 0], [0, -1, -2, -3]),     // Left
+            ([0, -1, -2, -3], [0, -1, -2, -3]),  // Up-Left
+            ([0, -1, -2, -3], [0, 0, 0, 0]),     // Up
+            ([0, -1, -2, -3], [0, 1, 2, 3])      // Up-Right
+        };
+
+        for (int i = 0; i < inputLines.Count(); i++)
+        {
+            for (int j = 0; j < inputLines[i].Count(); j++)
+            {
+                char currentChar = inputLines[i][j];
+                if (currentChar != xmasChars[0])
+                {
+                    continue;
+                }
+
+                for (int k = 0; k < checkOffsets.Count(); k++)
+                {
+                    if (check(i, j, checkOffsets[k], inputLines, xmasChars))
+                    {
+                        xmasCount += 1;
+                    }
+                }
+            }
+        }
+
+        return xmasCount;
+    }
+
+    public static int partTwo(string filePath)
+    {
+        int xmasCount = 0;
+        char[] xmasChars = ['M','A','S'];
+        string[] inputLines = File.ReadAllLines(filePath);
+        List<(List<int>, List<int>)> checkOffsets = new()
+        {
+            ([-1, 0, 1], [-1, 0, 1]),      // NW to SE
+            ([1, 0, -1], [-1, 0, 1])       // SW to NE
+        };
+
+        for (int i = 0; i < inputLines.Count(); i++)
+        {
+            for (int j = 0; j < inputLines[i].Count(); j++)
+            {
+                char currentChar = inputLines[i][j];
+                if (currentChar != 'A')
+                {
+                    continue;
+                }
+
+                bool nwToSeForward = check(i, j, checkOffsets[0], inputLines, ['M', 'A', 'S']);
+                bool nwToSeReverse = check(i, j, checkOffsets[0], inputLines, ['S', 'A', 'M']);
+                bool nwToSe = nwToSeForward || nwToSeReverse;
+
+                bool swToNeForward = check(i, j, checkOffsets[1], inputLines, ['M', 'A', 'S']);
+                bool swToNeReverse = check(i, j, checkOffsets[1], inputLines, ['S', 'A', 'M']);
+                bool swToNe = swToNeForward || swToNeReverse;
+
+                xmasCount += nwToSe && swToNe ? 1 : 0;
+            }
+        }
+
+        return xmasCount;
+    }
+
+    public static bool check(int rowIndex, int colIndex, (List<int>, List<int>) checkOffsets, string[] inputLines, char[] desiredChars)
+    {
+        List<int> rowOffsets = checkOffsets.Item1;
+        List<int> colOffsets = checkOffsets.Item2;
+
+        int rowMax = inputLines.Count();
+        int colMax = inputLines[0].Count();  // hope rows aren't variable in length!
+
+        if(rowIndex + rowOffsets.Max() >= rowMax || rowIndex + rowOffsets.Min() < 0
+            || colIndex + colOffsets.Max() >= colMax || colIndex + colOffsets.Min() < 0 )
+        {
+            return false;
+        }
+
+        List<char> asFound = new();
+        for (int i = 0; i < checkOffsets.Item1.Count; i++)
+        {
+            asFound.Add(inputLines[rowIndex + rowOffsets[i]][colIndex + colOffsets[i]]);
+        }
+
+        return asFound.SequenceEqual(desiredChars);
+    }
+}
---
+++ 2024/day05/Program.cs
@@ -0,0 +1,144 @@
+﻿namespace day05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(partOne("input.txt"));
+        Console.WriteLine(partTwo("input.txt"));
+    }
+
+    static int partOne(string dataFile)
+    {
+        List<string> rawInput = File.ReadAllLines(dataFile).ToList();
+        int divisionPoint = rawInput.FindIndex(x => x == string.Empty);
+        List<string> rawRules = rawInput.Take(divisionPoint).ToList();
+        List<string> rawPageLists = rawInput.Skip(divisionPoint + 1).ToList();
+
+        List<(int, int)> rules = parseRules(rawRules);
+        List<List<int>> pageLists = parsePageLists(rawPageLists);
+
+        int result = 0;
+
+        foreach (List<int> pageList in pageLists)
+        {
+            List<(int, int)> relevantRules = rules
+                .Where(r => pageList.Contains(r.Item1) && pageList.Contains(r.Item2))
+                .ToList();
+
+            List<int> expectedOrder = new();
+            int requiredRuleCount = pageList.Count() - 1;
+            int currentPageIndex = 0;
+
+            while (requiredRuleCount >= 0)
+            {
+                // Console.WriteLine($"{requiredRuleCount} | {currentPageIndex} | {pageList[currentPageIndex]} | {relevantRules.Where(r => r.Item1 == pageList[currentPageIndex]).Count()} | {string.Join(", ", relevantRules.Where(r => r.Item1 == pageList[currentPageIndex]))}");
+
+                if (relevantRules.Where(x => x.Item1 == pageList[currentPageIndex]).Count() == requiredRuleCount)
+                {
+                    expectedOrder.Add(pageList[currentPageIndex]);
+                    requiredRuleCount -= 1;
+                }
+
+                currentPageIndex = currentPageIndex == (pageList.Count() - 1) ? 0 : currentPageIndex + 1;
+            }
+
+            bool isCorrect = pageList.SequenceEqual(expectedOrder);
+            // Console.WriteLine($"{isCorrect} | {string.Join(", ", pageList)} | {string.Join(", ", expectedOrder)}");
+
+            if (isCorrect)
+            {
+                bool oddListCount = pageList.Count() % 2 != 0;
+                int middleIndex = pageList.Count() % 2 != 0 ? pageList.Count() / 2 : pageList.Count() / 2 - 1;
+
+                // Console.WriteLine($"{pageList.Count} | {middleIndex} | {pageList[middleIndex]} | {string.Join(", ", pageList)}");
+                result += pageList[middleIndex];
+            }
+        }
+
+        return result;
+    }
+
+    static int partTwo(string dataFile)
+    {
+        List<string> rawInput = File.ReadAllLines(dataFile).ToList();
+        int divisionPoint = rawInput.FindIndex(x => x == string.Empty);
+        List<string> rawRules = rawInput.Take(divisionPoint).ToList();
+        List<string> rawPageLists = rawInput.Skip(divisionPoint + 1).ToList();
+
+        List<(int, int)> rules = parseRules(rawRules);
+        List<List<int>> pageLists = parsePageLists(rawPageLists);
+
+        int result = 0;
+
+        foreach (List<int> pageList in pageLists)
+        {
+            List<(int, int)> relevantRules = rules
+                .Where(r => pageList.Contains(r.Item1) && pageList.Contains(r.Item2))
+                .ToList();
+
+            List<int> expectedOrder = new();
+            int requiredRuleCount = pageList.Count() - 1;
+            int currentPageIndex = 0;
+
+            while (requiredRuleCount >= 0)
+            {
+                // Console.WriteLine($"{requiredRuleCount} | {currentPageIndex} | {pageList[currentPageIndex]} | {relevantRules.Where(r => r.Item1 == pageList[currentPageIndex]).Count()} | {string.Join(", ", relevantRules.Where(r => r.Item1 == pageList[currentPageIndex]))}");
+
+                if (relevantRules.Where(x => x.Item1 == pageList[currentPageIndex]).Count() == requiredRuleCount)
+                {
+                    expectedOrder.Add(pageList[currentPageIndex]);
+                    requiredRuleCount -= 1;
+                }
+
+                currentPageIndex = currentPageIndex == (pageList.Count() - 1) ? 0 : currentPageIndex + 1;
+            }
+
+            bool isCorrect = pageList.SequenceEqual(expectedOrder);
+            // Console.WriteLine($"{isCorrect} | {string.Join(", ", pageList)} | {string.Join(", ", expectedOrder)}");
+
+            if (!isCorrect)
+            {
+                bool oddListCount = expectedOrder.Count() % 2 != 0;
+                int middleIndex = expectedOrder.Count() % 2 != 0 ? expectedOrder.Count() / 2 : expectedOrder.Count() / 2 - 1;
+
+                // Console.WriteLine($"{expectedOrder.Count} | {middleIndex} | {expectedOrder[middleIndex]} | {string.Join(", ", expectedOrder)}");
+                result += expectedOrder[middleIndex];
+            }
+        }
+
+        return result;
+    }
+
+    static List<(int, int)> parseRules(List<string> rawRules)
+    {
+        List<(int, int)> rules = new();
+        foreach (string rawRule in rawRules)
+        {
+            string[] ruleElements = rawRule.Split('|');
+            bool elementOneResult = int.TryParse(ruleElements[0], out int elementOne);
+            bool elementTwoResult = int.TryParse(ruleElements[1], out int elementTwo);
+
+            if(!elementOneResult || !elementTwoResult)
+            {
+                Console.WriteLine($"Unable to parse rule: {rawRule}");
+                continue;
+            }
+
+            rules.Add((elementOne, elementTwo));
+        }
+
+        return rules;
+    }
+
+    static List<List<int>> parsePageLists(List<string> rawPageLists)
+    {
+        List<List<int>> pageLists = new();
+        foreach (string rawPageList in rawPageLists)
+        {
+            pageLists.Add(rawPageList.Split(',').Select(x => int.Parse(x)).ToList());
+        }
+
+        return pageLists;
+    }
+}
